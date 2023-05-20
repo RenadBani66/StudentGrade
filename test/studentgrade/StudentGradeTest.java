1c8da142d30cdbdfb6219b5b9cf464925a88d069
RenadBani66 <RenadBani66@gmail.com>
2023-05-20 22:07:51 +0300

    upgrade the JUnit from 3.8 to 4.2 and add class in new version like import org.junit.Test;and update some code like add @Test and use the Assert and delete TestCase from framework as new verzion requred

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,35 +1,34 @@
 package studentgrade;
 
-import junit.framework.TestCase;
+import junit.framework.Assert;
+import org.junit.Test;
 
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
+public class StudentGradeTest {
     
-    public StudentGradeTest(String testName) {
-        super(testName);
-    }
-
+    @Test
     public void testGetGrade95() {
         System.out.println("getGrade 95");
         int mark = 95;
         String expResult = "A";
         String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+        Assert.assertEquals(expResult, result);
     }
 
+    @Test
     public void testGetGrade85() {
         System.out.println("getGrade 85");
         int mark = 85;
         String expResult = "B";
         String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+        Assert.assertEquals(expResult, result);
     }
     
+    @Test
     public void testGetGrade75() {
-        System.out.println("getGrade 85");
+        System.out.println("getGrade 75");
         int mark = 75;
         String expResult = "F";
         String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+        Assert.assertEquals(expResult, result);
     }
 }
